ccdfaf0bb714b949a76158a5a440adf1a13daa3a
Pedro Coelho Torres <pedro.coelhotorres@bnc.ca>
2018-11-26 10:57:04 -0500

    Organize file structure

---
+++ src/main/java/com/pedrocoelho/coding/App.java
@@ -0,0 +1,8 @@
+package com.pedrocoelho.coding;
+
+public class App {
+
+    public static void main(String [] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/pedrocoelho/coding/AppTest.java
@@ -0,0 +1,11 @@
+package com.pedrocoelho.coding;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        System.out.println("Hello world!");
+    }
+}
